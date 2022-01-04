d2317ea06c73b2816b9cdc177d2c2c4c6357bce0
Chus Escudero <45289041+chus-95@users.noreply.github.com>
2022-01-04 13:37:28 +0100

    update todolist.py
    line 15 error whitespace

--- src/todoList.py
+++ src/todoList.py
@@ -9,11 +9,10 @@ from botocore.exceptions import ClientError
 
 def get_table(dynamodb=None):
     if not dynamodb:
-        URL = "http://localhost:8000"
+        URL = 'http://localhost:8000'
         if URL:
             print('URL dynamoDB:'+URL)
-            boto3.client = functools.partial(boto3.client, 
-                                             endpoint_url=URL)
+            boto3.client = functools.partial(boto3.client, endpoint_url=URL)
             boto3.resource = functools.partial(boto3.resource,
                                                endpoint_url=URL)
         dynamodb = boto3.resource("dynamodb")
