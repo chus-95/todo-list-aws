6ec5adc8f83d16666b2b13bc4559d066ffad5cee
Chus Escudero <45289041+chus-95@users.noreply.github.com>
2022-01-04 13:18:21 +0100

    update todolist.py
    drop region_name cuz line is too long.

--- src/todoList.py
+++ src/todoList.py
@@ -13,9 +13,9 @@ def get_table(dynamodb=None):
             URL = "http://localhost:8000"
             if URL:
                 print('URL dynamoDB:'+URL)
-                boto3.client = functools.partial(boto3.client, endpoint_url=URL,region_name='us-east-1')
+                boto3.client = functools.partial(boto3.client, endpoint_url=URL)
                 boto3.resource = functools.partial(boto3.resource,
-                                                   endpoint_url=URL,region_name='us-east-1')
+                                                   endpoint_url=URL)
             dynamodb = boto3.resource("dynamodb",region_name='us-east-1')
              # fetch todo from the database
             table= dynamodb.Table(os.environ['DYNAMODB_TABLE'])
