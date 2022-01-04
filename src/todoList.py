e6483be916f4c27c2124adc3dc8ad9df5ede870d
Chus Escudero <45289041+chus-95@users.noreply.github.com>
2022-01-04 11:46:32 +0100

    update todolist.py
    comeback first commit

--- src/todoList.py
+++ src/todoList.py
@@ -6,21 +6,17 @@ import json
 import functools
 from botocore.exceptions import ClientError
 
-
 def get_table(dynamodb=None):
     if not dynamodb:
-        URL = "http://localhost:8000"
+        URL = os.environ['ENDPOINT_OVERRIDE']
         if URL:
             print('URL dynamoDB:'+URL)
             boto3.client = functools.partial(boto3.client, endpoint_url=URL)
             boto3.resource = functools.partial(boto3.resource,
                                                endpoint_url=URL)
-            
-        dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
-        # fetch todo from the database
-        table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-    else:
-        print("dynamodb table exists")
+        dynamodb = boto3.resource("dynamodb")
+    # fetch todo from the database
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     return table
 
 
