855d962cbfb6534069d308b639722369d6f64a7c
Chus Escudero <45289041+chus-95@users.noreply.github.com>
2022-01-04 13:34:06 +0100

    update todolist.py
    due error we come back to previous code changing localhost

--- src/todoList.py
+++ src/todoList.py
@@ -9,19 +9,16 @@ from botocore.exceptions import ClientError
 
 def get_table(dynamodb=None):
     if not dynamodb:
-        try:
-            URL = "http://localhost:8000"
-            if URL:
-                print('URL dynamoDB:'+URL)
-                boto3.client = functools.partial(boto3.client, endpoint_url=URL)
-                boto3.resource = functools.partial(boto3.resource,
-                                                   endpoint_url=URL)
-            dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
-            # fetch todo from the database
-            table= dynamodb.Table(os.environ['DYNAMODB_TABLE'])         
-        except ClientError as e:
-            print(e.response['Error']['Message'])
-            table= None     
+        URL = "http://localhost:8000"
+        if URL:
+            print('URL dynamoDB:'+URL)
+            boto3.client = functools.partial(boto3.client, 
+                                             endpoint_url=URL)
+            boto3.resource = functools.partial(boto3.resource,
+                                               endpoint_url=URL)
+        dynamodb = boto3.resource("dynamodb")
+    # fetch todo from the database
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     return table
 
 
