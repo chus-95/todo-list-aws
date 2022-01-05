97a8f9ba2906ae646663bb099dadefae85bba8f5
Chus Escudero <45289041+chus-95@users.noreply.github.com>
2022-01-05 16:46:46 +0100

    update todoapitest.py
    add some prints to detect why is failing integration_test

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -53,11 +53,14 @@ class TestApi(unittest.TestCase):
         print('---------------------------------------')
         print('Starting - integration test Add TODO')
         url = BASE_URL+"/todos"
+        print(url)
         data = {
          "text": "Integration text example"
         }
+        print(data)
         response = requests.post(url, data=json.dumps(data))
         json_response = response.json()
+        print(json_response)
         print('Response Add Todo: '+ json_response['body'])
         jsonbody= json.loads(json_response['body'])
         ID_TODO = jsonbody['id']
